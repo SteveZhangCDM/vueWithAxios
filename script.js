73d659228384ca648fbaa58be00a0f477db85f98
Steve_Zhang <wentaozhang0305@gmail.com>
2020-05-10 20:10:59 -0700

    played with axios for the request

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ var app = new Vue({
     message: 'Hello Vue!',
   },
 });
+
+document.querySelector('.get').onclick = function () {
+  axios.get('https://autumnfish.cn/api/joke/list?num=6').then(
+    function (response) {
+      console.log(response);
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
+};
+
+document.querySelector('.post').onclick = function () {
+  axios
+    .post('https://autumnfish.cn/api/user/reg', { username: 'HYAKUYA' })
+    .then(
+      function (response) {
+        console.log(response);
+      },
+      function (err) {
+        console.log(err);
+      }
+    );
+};
